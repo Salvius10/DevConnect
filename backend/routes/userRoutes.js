9d555d43788268e62798e33110d2811b47bf9c49
Salvius10 <melvinsalvius.i@gmail.com>
2025-11-25 14:41:02 +0530

    profilePic backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,23 @@
 const express=require("express")
 const router=express.Router()
+const upload=require("../middleware/upload")
 const {getUserProfiles,updateProfile}=require("../controllers/userController")
 const authMiddleware = require("../middleware/authMiddleware")
-
+const User=require("../models/User")
 router.get("/:id",getUserProfiles)
 router.put("/update",authMiddleware,updateProfile)
-
+router.post("upload-photo",authMiddleware,upload.single("photo"),async (req,res)=>{
+    try {
+        const photoPath=req.file.path
+        const user=await User.findByIdAndUpdate(
+            req.user,
+            {profilephoto:photoPath},
+            {new:true}
+        )
+        res.json(user)    
+    } catch (err) {
+        res.status(500).json({error:err.message})
+    }
+    
+})
 module.exports=router
